9d1d414393155cf870f5e82d2822bb63eaf943db
Gregory-Schwing-MD-PhD <gregory.schwing@med.wayne.edu>
2026-02-10 10:12:45 -0500

    tried constraint file

--- docker/build_spineps.sh
+++ docker/build_spineps.sh
@@ -5,18 +5,15 @@ set -e
 
 DOCKER_USERNAME="go2432"
 PROJECT_NAME="spineps-lstv"
-VERSION="1.0"
 
 echo "Building SPINEPS container (NO CACHE)..."
 
 docker build --no-cache \
     -f docker/Dockerfile.spineps \
-    -t ${DOCKER_USERNAME}/${PROJECT_NAME}:${VERSION} \
     -t ${DOCKER_USERNAME}/${PROJECT_NAME}:latest \
     .
 
 echo "Pushing to Docker Hub..."
-docker push ${DOCKER_USERNAME}/${PROJECT_NAME}:${VERSION}
 docker push ${DOCKER_USERNAME}/${PROJECT_NAME}:latest
 
 echo "Done! Container: ${DOCKER_USERNAME}/${PROJECT_NAME}:latest"
